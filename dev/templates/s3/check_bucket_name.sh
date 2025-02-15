c854c61d68870476d93ac95268d5047136819d24
Usman Butt <usmanakramb@gmail.com>
2025-02-14 22:00:16 -0600

    fix script

--- dev/templates/s3/check_bucket_name.sh
+++ dev/templates/s3/check_bucket_name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract the bucket name from the Terraform plan JSON
-bucket_name=$(echo "$ENV0_TF_PLAN_JSON" | jq -r '.resource_changes[].change.after.bucket')
+bucket_name=$(echo "$ENV0_TF_PLAN_JSON" | jq -r '.resource_changes[] | select(.type == "aws_s3_bucket" and .change.actions[0] == "create") | .change.after.bucket')
 
 # Check if the bucket name starts with "env0"
 if [[ ! "$bucket_name" =~ ^env0.* ]]; then
