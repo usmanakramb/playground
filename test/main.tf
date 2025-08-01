173a36de0a34f41f0c76b62f43f652d0cd99b684
Usman <usmanakramb@gmail.com>
2025-08-01 15:26:09 -0500

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -27,14 +27,35 @@ data "env0_project" "default_project" {
 #  name = "Secret Key"
 #}
 
-resource "env0_template" "example" {
-  name                = "usman template"
-  description         = "usman template"
+resource "env0_template" "usman_template" {
+  name                = "usman-template"
+  description         = "usman-template"
   repository          = "https://github.com/env0/templates"
   path                = "aws/hello-world"
   type                = "terraform"
   terraform_version   = "1.5.7"
-#  ssh_keys           = [data.env0_ssh_key.my_key]
+}
+
+resource "env0_environment" "usman_provider_example" {
+  name        = "test-env-provider"
+  project_id  = data.env0_project.default_project.id
+  template_id = env0_template.usman_template.id
+
+  configuration {
+    name = "test-var1"
+    value = "test-value1"
+    type = "terraform"
+    is_read_only = true
+    is_required = true
+  }
+
+  configuration {
+    name = "test-var2"
+    value = "test-value2"
+    type = "terraform"
+    is_read_only = true
+    is_required = true
+  }
 }
 
 #resource "env0_template_project_assignment" "assignment" {
@@ -43,5 +64,5 @@ resource "env0_template" "example" {
 #}
 
 output "template_id" {
-    value = env0_template.example.id
+    value = env0_template.usman_template.id
 }
